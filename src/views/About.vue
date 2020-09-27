6ad2d3d6ad5c7fd53a58a9a72d8dd196a6cc77c7
xiaobian233 <1282845084@qq.com>
2020-09-27 15:16:07 +0800

    add

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,25 +1,84 @@
 <template>
-<div class="about">
-    <h1>This is an about page</h1>
-</div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <button @click="addClick">点击click</button>
+        <div>
+            <p>foo.count : {{ foo.count }}</p>
+            <p>data.count : {{ data.count }}</p>
+            <p>newState.count : {{ count }}</p>
+            <input v-model="demoValue" />
+            <p>shallowRefState.value : {{ shallowRefState }}</p>
+            <p>triggerRefState.value : {{ triggerRefState.text }}</p>
+        </div>
+        <midde>
+
+            <template v-slot:slot="{names}" style="display: block;">
+                <h2>this is midden slov---------------{{names}}</h2>
+            </template>
+
+            <template v-slot:header style="display: block;">
+                <h1>Here might be a page title</h1>
+            </template>
+
+        </midde>
+    </div>
 </template>
 
 <script>
-const ThemeSymbol = Symbol();
-import {
-    provide,
-    onMounted,
-    inject
-} from "vue";
-export default {
-    setup(props) {
-        // const injectStr = inject(ThemeSymbol, "brak");
-        // return {
-        //     injectStr,
-        // };
-        onMounted(() => {
-            provide(ThemeSymbol, "brak");
-        });
-    },
-};
+    import * as _vue from "vue";
+    export default {
+        name: "about",
+        data() {
+            return {
+                header: "header"
+            }
+        },
+        comments: {
+            midde: _vue.defineAsyncComponent(() => import("./midde.vue"))
+        },
+        setup(props) {
+            const foo = { count: 0 }
+            const data = _vue.reactive(foo)
+            const state = _vue.reactive({ count: 0, is: true })
+            const { count } = _vue.toRefs(state) //可以在不失去响应式的情况下破坏结构
+            const demoValue = createCustomRef("v-model 解析")
+            const addClick = () => {
+                foo.count++
+                data.count++
+                count.value++
+                shallowRefState.value++
+                log(_vue.toRaw(data), _vue.toRaw(foo), count.value);
+            }
+            const shallowRefState = _vue.shallowRef({})
+            shallowRefState.value = 0
+
+            const triggerRefState = _vue.shallowRef({ text: "hello Vue" })
+            _vue.watchEffect(() => { log(triggerRefState.value.text, "watchEffect triggerRefState") })
+            triggerRefState.value.text = "hello world!"
+            _vue.triggerRef(triggerRefState, "triggerRefState")
+
+
+            return { data, foo, addClick, count, demoValue, shallowRefState, triggerRefState }
+
+            //ref 的防抖设置
+            function createCustomRef(value, time = 200) {
+                let timeout = null;
+                return _vue.customRef((track, trigger) => {
+                    return {
+                        get() {
+                            track()
+                            return value
+                        },
+                        set(newValue) {
+                            clearTimeout(newValue)
+                            timeout = setTimeout(() => {
+                                value = newValue
+                                trigger()
+                            }, time)
+                        }
+                    }
+                })
+            }
+        },
+    };
 </script>
---
+++ src/views/midde.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+        <h1>This is an midde page</h1>
+        <slot name="slot" :slot="names"></slot>
+        <header>
+            <slot name="header"></slot>
+        </header>
+        <!-- <slot></slot> -->
+        <!-- <slot></slot> -->
+    </div>
+</template>
+
+<script>
+    import * as _vue from "vue";
+    export default {
+        name: "midde",
+        setup(props) {
+            const names = "v-slot"
+            return { names }
+        },
+    };
+</script>
